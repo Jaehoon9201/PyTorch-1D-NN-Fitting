7d1b91e48851b28a47e041781c0b5425f40245ca
Jaehoon Shim <71545160+Jaehoon9201@users.noreply.github.com>
2024-02-07 10:35:05 +0900

    Update ex_y1dx_y2dx.py

--- DerivationExample/ex_y1dx_y2dx.py
+++ DerivationExample/ex_y1dx_y2dx.py
@@ -1,48 +1,36 @@
-# Reference: https://medium.com/@rizqinur2010/partial-derivatives-chain-rule-using-torch-autograd-grad-a8b5917373fa
-
 import torch
-import numpy as np
+from torch.autograd import grad
 
-# 모델 정의
+device = 'cuda'
+# .pt load
 class SimpleModel(torch.nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
-        self.linear = torch.nn.Linear(3, 2)
+        self.linear = torch.nn.Linear(10, 2)
 
     def forward(self, x):
         return self.linear(x)
 
 # 모델 및 입력 데이터 생성
 model  = SimpleModel()
-x      = torch.tensor([[2.0, 3.0, 1.0], [1.0, 2.0, 0.5]], requires_grad=True)
-target = torch.tensor([[1.0, 0.0], [0.0, 0.5]])
+model = model.to(device)
+model = model.eval()
+# ex data
+x = torch.tensor([[4.1547e-03,  4.4907e-02, -2.7384e-03, 9.9574e-04, 3.1319e-02,
+                   9.3274e-02, -8.1103e-01, -2.3757e-01, 1.2081e-01, 1.0000e+00]],
+                 device='cuda:0', requires_grad=True)
 
-# 출력 계산
-y = model(x)
+output    = model(x.to(device))
+print(output)
+print(output[0][0])
+print(output[0][1])
 
-# 로스 계산
-L = torch.nn.functional.mse_loss(y, target)
+# torch.autograd.grad --> calculating gradients
+gradients  = torch.autograd.grad(output.to(device), x.to(device), grad_outputs=torch.tensor([[1, 1]]).to(device), retain_graph = True)
+gradients1 = torch.autograd.grad(output[0][0].to(device), x.to(device), grad_outputs=torch.tensor(1).to(device), retain_graph = True)
+gradients2 = torch.autograd.grad(output[0][1].to(device), x.to(device), grad_outputs=torch.tensor(1).to(device), retain_graph = True)
 
-dydx               = torch.autograd.grad(outputs=y, inputs=x, grad_outputs=torch.ones_like(y), create_graph=True, retain_graph=True)  # dy/dx * grad_output = dy/dx
-dLdy               = torch.autograd.grad(outputs=L, inputs=y                                 , create_graph=True, retain_graph=True)  # dL/dy
-
-# 그래디언트 계산 방법 1, 2
-dLdx               = torch.autograd.grad(outputs=y, inputs=x, grad_outputs=dLdy[0]                              , retain_graph=True)  # dy/dx * dL/dy = dL/dx
-dLdx2              = torch.autograd.grad(outputs=L, inputs=x                                                    , retain_graph=True)  # dL/dx
-
-# 그래디언트 계산 방법 3
-L.backward(retain_graph=True)
-dLdx3 = x.grad  # cf. y.grad = dL/dy
-
-# 결과 출력 1
-print("dy/dx                   :\n", dydx[0].detach().numpy()  ,'\n')
-print("dL/dy                   :\n", dLdy[0].detach().numpy()  ,'\n')
-print("dL/dx (=dy/dx * dL/dy)  :\n", dLdx[0].detach().numpy()  ,'\n')
-# print('dy/dx * dL/dy           :\n', np.dot(  np.transpose(dLdy[0].detach().numpy())   ,   (dydx[0].detach().numpy())   ) ,'\n') # ???
-
-# 결과 출력 2
-print("dL/dx                   :\n", dLdx2[0].detach().numpy() ,'\n')
-
-# 결과 출력 3
-print("dL/dx                   :\n", dLdx3.detach().numpy()    ,'\n')
-
+# outputs
+print("dy/dx  : \n", gradients)
+print("dy1/dx : \n", gradients1)
+print("dy2/dx : \n", gradients2)
